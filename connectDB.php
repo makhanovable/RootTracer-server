4e38e2d3e78d9bcb1afd1c344d5c8543fd747f62
Makhanov Madiyar <makhanovable@gmail.com>
2018-03-26 22:28:45 +0600

    connection to Database

--- connectDB.php
+++ connectDB.php
@@ -1,7 +1,7 @@
 <?php
 	$host = "localhost";
 	$user = "id3116113_hanz";
-	$password = "arduino121232";
+	$password = "password";
 	$database = "id3116113_login";
 	$connection = mysqli_connect($host, $user, $password, $database);
 	if(!$connection) {
